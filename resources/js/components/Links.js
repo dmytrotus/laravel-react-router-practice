042db829ccf929d1b94b6e824a3777917cd5e008
Dmytro Tus <dmytrotus@gmail.com>
2020-03-03 22:14:52 +0100

    make direct url work

--- resources/js/components/Links.js
+++ resources/js/components/Links.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Links(){
 	return (
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-Route::get('/', 'ReactController@index');
+Route::get('/{any}', 'ReactController@index')->where('any', '.*');
